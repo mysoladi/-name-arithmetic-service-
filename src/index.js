a4f2a472555dc61dc1a2c3ed928620a99edf1a66
mysoladi <mysoladi@iu.edu>
2024-02-08 22:40:30 -0500

    .

--- src/index.js
+++ src/index.js
@@ -19,4 +19,16 @@ app.get('/add', (req, res) => {
     }
 });
 
+app.get('/sub', (req, res) => {
+    const num1 = parseFloat(req.query.num1);
+    const num2 = parseFloat(req.query.num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        res.status(400).send('Invalid numbers provided');
+    } else {
+        const sum = num1 - num2;
+        res.json({ result: sum });
+    }
+});
+
 app.listen(port);
